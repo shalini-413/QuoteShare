c874246bdfd2d67a013929a94d97a8868b8b37df
AjeetRawat3020 <ajeetsinghrawat1999@gmail.com>
2025-06-10 18:15:48 +0530

    Day 1: Frontend Vite + Tailwind setup

---
+++ quoteshare-client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+function App() {
+  return (
+    <>
+      <div>
+        <p className="text-center p-10 text-2xl font-bold text-blue-600">
+          Welcome to QuoteShare ✨
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
