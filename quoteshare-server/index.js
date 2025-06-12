019134901d6cd846db665d77dc166a1426032705
Shalini <shalinishalu462002@gmail.com>
2025-06-12 17:32:59 +0530

    Day 2: Auth API (Register/Login)

--- quoteshare-server/index.js
+++ quoteshare-server/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 require("dotenv").config();
+
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -8,10 +11,14 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
 app.get("/", (req, res) => {
-  res.send("Welcome to QuoteShare API");
+  res.send("Welcome to the QuoteShare Backend API 🚀");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+app.use("/api/auth", authRoutes);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => app.listen(PORT, () => console.log(`Server on http://localhost:${PORT}`)))
+  .catch((err) => console.error(err));
